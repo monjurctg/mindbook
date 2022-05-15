f613abf9619ade1c1ded7093f6bd6f5a7eead184
monjurpy.06 <monjurpy.06@gmail.com>
2022-05-15 14:54:09 +0600

    update

--- index.js
+++ index.js
@@ -13,15 +13,10 @@ const socket = require("socket.io");
 // const { message } = require("statuses");
 // const { JWT_SECRET } = require("./controlers/variables");
 
-
 // const io = new Server(expressServer);
 
 const PORT = process.env.PORT || 5000;
 const ORIGIN = process.env.ORIGIN || `http://localhost:3000`;
-
-
-
-
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -30,7 +25,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 // simple route
 
-const uri = "mongodb+srv://chatApp:monjur123@cluster0.pvcyw.mongodb.net/Chat-App?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://chatApp:monjur123@cluster0.pvcyw.mongodb.net/Chat-App?retryWrites=true&w=majority";
 // const uri = 'mongodb://localhost:27017/myapp'
 
 mongoose
@@ -46,7 +42,6 @@ mongoose
     process.exit();
   });
 app.get("/", (req, res) => {
-
   res.sendFile(__dirname + "/index.html");
 });
 // route setup
@@ -56,30 +51,29 @@ app.use("/api/messages", messageRouter);
 // set port, listen for requests
 
 const server = app.listen(PORT, () => {
-
   console.log(`Server is running on. http://localhost:${PORT}`);
 });
 
-
-
-const io = socket(server,{
-  cors:{
-    origin:ORIGIN,
-    Credential:true
-  }
-})
+const io = socket(server, {
+  cors: {
+    origin: ORIGIN?ORIGIN:'https://chaat-app.netlify.app/',
+    Credential: true,
+  },
+});
 // console.log(io,"io")
-global.onlineUsers = new Map()
+global.onlineUsers = new Map();
 
-io.on('connection',(socket)=>{
-  global.chatSchoket = socket
-  socket.on('add-user',(userId)=>{
-    onlineUsers.set(userId,socket.id)
+io.on("connection", (socket) => {
+  global.chatSchoket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
   });
-  socket.on("send-msg",(data)=>{
-    const sendUserSocket = onlineUsers.get(data.to)
-    if(sendUserSocket){
-      socket.to(sendUserSocket).emit('msg-recive',data.msg)
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    console.log(onlineUsers, "onkdfkdj");
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recive", data.msg);
     }
-  })
-})
+  });
+});
